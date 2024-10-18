bf4974c31b505f5947c7f620b1f7406bc4e6d00f
José Valderrama <josevalderrama18@gmail.com>
2024-10-17 21:45:55 -0300

    use original problem instead of solution with class

--- BootcampSesion5/BootcampSesion5/Poor performance/PoorPerformanceExample.swift
+++ BootcampSesion5/BootcampSesion5/Poor performance/PoorPerformanceExample.swift
@@ -9,62 +9,16 @@ import SwiftUI
 
 // **** problema original donde todas visibles las celdas se recargan al cambiar una sola
 
-//struct PoorPerformanceExample: View {
-//    @State private var items: [Item] = (1...100).map { Item(id: $0, name: "Item \($0)", isSelected: false) }
-//    
-//    var body: some View {
-//        let _ = Self._printChanges()
-//        
-//        NavigationView {
-//            List {
-//                ForEach($items) { $item in
-//                    ItemRowView(item: $item)
-//                }
-//            }
-//            .navigationTitle("Items")
-//        }
-//    }
-//}
-//
-//struct Item: Identifiable {
-//    let id: Int
-//    var name: String
-//    var isSelected: Bool
-//}
-//
-//struct ItemRowView: View {
-//    @Binding var item: Item
-//    
-//    var body: some View {
-//        let _ = Self._printChanges()
-//        print("Renderizando ItemRowView para \(item.name)")
-//        
-//        return HStack {
-//            Text(item.name)
-//            Spacer()
-//            Button(action: {
-//                item.isSelected.toggle()
-//            }) {
-//                Image(systemName: item.isSelected ? "checkmark.circle.fill" : "circle")
-//            }
-//        }
-//        .padding()
-//    }
-//}
-
-
-// **** fixed opcion 1, usar clase
-
 struct PoorPerformanceExample: View {
     @State private var items: [Item] = (1...100).map { Item(id: $0, name: "Item \($0)", isSelected: false) }
-
+    
     var body: some View {
         let _ = Self._printChanges()
-
+        
         NavigationView {
             List {
-                ForEach(items) { item in
-                    AnyView(ItemRowView(item: item))
+                ForEach($items) { $item in
+                    ItemRowView(item: $item)
                 }
             }
             .navigationTitle("Items")
@@ -72,29 +26,23 @@ struct PoorPerformanceExample: View {
     }
 }
 
-class Item: Identifiable, ObservableObject {
+struct Item: Identifiable {
     let id: Int
     var name: String
-    @Published var isSelected: Bool
-
-    init(id: Int, name: String, isSelected: Bool) {
-        self.id = id
-        self.name = name
-        self.isSelected = isSelected
-    }
+    var isSelected: Bool
 }
 
 struct ItemRowView: View {
-    @ObservedObject var item: Item
-
+    @Binding var item: Item
+    
     var body: some View {
         let _ = Self._printChanges()
-
+        print("Renderizando ItemRowView para \(item.name)")
+        
         return HStack {
             Text(item.name)
             Spacer()
             Button(action: {
-                print("si se ejecuta")
                 item.isSelected.toggle()
             }) {
                 Image(systemName: item.isSelected ? "checkmark.circle.fill" : "circle")
@@ -103,6 +51,58 @@ struct ItemRowView: View {
         .padding()
     }
 }
+
+
+// **** fixed opcion 1, usar clase
+
+//struct PoorPerformanceExample: View {
+//    @State private var items: [Item] = (1...100).map { Item(id: $0, name: "Item \($0)", isSelected: false) }
+//
+//    var body: some View {
+//        let _ = Self._printChanges()
+//
+//        NavigationView {
+//            List {
+//                ForEach(items) { item in
+//                    ItemRowView(item: item)
+//                }
+//            }
+//            .navigationTitle("Items")
+//        }
+//    }
+//}
+//
+//class Item: Identifiable, ObservableObject {
+//    let id: Int
+//    var name: String
+//    @Published var isSelected: Bool
+//
+//    init(id: Int, name: String, isSelected: Bool) {
+//        self.id = id
+//        self.name = name
+//        self.isSelected = isSelected
+//    }
+//}
+//
+//struct ItemRowView: View {
+//    @ObservedObject var item: Item
+//
+//    var body: some View {
+//        let _ = Self._printChanges()
+//
+//        return HStack {
+//            Text(item.name)
+//            Spacer()
+//            Button(action: {
+//                print("si se ejecuta")
+//                item.isSelected.toggle()
+//            }) {
+//                Image(systemName: item.isSelected ? "checkmark.circle.fill" : "circle")
+//            }
+//        }
+//        .padding()
+//    }
+//}
 
 // **** fixed opcion 2, usar EquatableView
 
